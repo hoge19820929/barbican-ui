4a806c7e53531767d5e9c350cc563b7b50d84a43
unknown <obefefai52@yahoo.co.jp>
2024-12-24 22:25:48 +0900

    fix enabled

--- barbican_ui/enabled/_4010_barbican_secrets_panel.py
+++ barbican_ui/enabled/_4010_barbican_secrets_panel.py
@@ -12,8 +12,7 @@
 
 # The slug of the panel to be added to HORIZON_CONFIG. Required.
 PANEL = 'secrets'
-# The slug of the panel group the PANEL is associated with.
-PANEL_GROUP = 'default'
+
 # The slug of the dashboard the PANEL associated with. Required.
 PANEL_DASHBOARD = 'barbican'
 
