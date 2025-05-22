363b833d6e221951f2057b6b8ba98c3973c6b4f2
unknown <obefefai52@yahoo.co.jp>
2025-05-22 21:38:33 +0900

    minor fix

--- barbican_ui/content/dashboard.py
+++ barbican_ui/content/dashboard.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 import horizon
 
 class BarbicanDashboard(horizon.Dashboard):
-    name = _("Kms")
+    name = _("KMS")
     slug = "kms"
 
 horizon.register(BarbicanDashboard)
